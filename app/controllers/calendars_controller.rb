f4cde0268e99e2479faf3f060e78f96053c0c2d4
123yuki <watagashi88@gmail.com>
2022-07-19 22:19:46 +0900

    Revert "Merge branch 'first_training' of https://github.com/123yuki/training_curriculum into first_training"
    This reverts commit e505aefe123f97eb47632bc1cb4ae9e9fc66f16d, reversing
    changes made to b6fec73dff6982469308dda5d9f563cc6b5d749b.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -3,17 +3,16 @@ class CalendarsController < ApplicationController
   # １週間のカレンダーと予定が表示されるページ
   def index
     get_week
-    #スネークケースに変更
-    @Plan = plan.new
+    @plan = Plan.new
   end
 
   # 予定の保存
   def create
-    plan.create(plan_params)
+    Plan.create(plan_params)
     redirect_to action: :index
   end
 
-  Private
+  private
 
   def plan_params
     params.require(:calendars).permit(:date, :plan)
@@ -28,14 +27,14 @@ class CalendarsController < ApplicationController
 
     @week_days = []
 
-    Plans = plan.where(date: @todays_date..@todays_date + 6)
+    plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
-      Todayplans = []
+      today_plans = []
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      Days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
       @week_days.push(days)
     end
 
